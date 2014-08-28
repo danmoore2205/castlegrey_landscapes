2868df692848a71dd138610092f30db8fd749f3b
Dan Moore <danmoore2205@gmail.com>
2014-08-28 16:10:30 +0200

    removing debugger route

--- app.rb
+++ app.rb
@@ -2,14 +2,10 @@ require 'sinatra'
 
 class App < Sinatra::Base
 
-    set :haml, { :format => :html5 }
+  set :haml, { :format => :html5 }
 
   get '/', :provides => :html do
     haml :index
-  end
-
-  get '/bar' do
-    settings.respond_to?(:haml).to_s
   end
 
 end
